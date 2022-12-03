5fa4018bf6a670c1ad2b4637b8c63a16e8908b91
anon-bianco <112154775+anon-bianco@users.noreply.github.com>
2022-12-03 14:55:26 +0530

    commit

---
+++ static_method/src/com/anon/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.anon;
+
+/**
+ *
+ * @author anonh
+ */
+public class Hello {
+    public static void drive() {
+        System.out.println("Let's go for a ride");
+    }
+    
+}
