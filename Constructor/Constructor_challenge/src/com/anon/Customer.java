d900aa123e252efd11ab45e41c426f4f99cfccc1
anon-bianco <112154775+anon-bianco@users.noreply.github.com>
2022-12-03 12:18:53 +0530

    Initial commit

---
+++ Constructor/Constructor_challenge/src/com/anon/Customer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.anon;
+
+/**
+ *
+ * @author anonh
+ */
+public class Customer {
+    private String name;
+    private double creditLimit = 989;
+    private String email;
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+    
+    
+    
+    
+    
+}
---
+++ Constructor/Constructor_challenge/src/com/anon/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.anon;
+
+/**
+ *
+ * @author anonh
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Customer obj1 = new Customer();
+        
+        System.out.println("The credit score is " + obj1.getCreditLimit());
+    }
+    
+}
