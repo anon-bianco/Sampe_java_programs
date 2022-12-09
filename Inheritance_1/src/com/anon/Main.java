0312d9bbe8cf14465d76464e9b515cd4c197e5ce
Anon Bianco <112154775+anon-bianco@users.noreply.github.com>
2022-12-09 06:59:23 +0530

    Add Inheritance example 1
    Calculator -> Sub -> Add

---
+++ Inheritance_1/src/com/anon/Main.java
@@ -0,0 +1,16 @@
+package com.anon;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Calculator obj1 = new Calculator();
+        
+        System.out.println(obj1.sub(5, 2));
+        System.out.println(obj1.add(25, 2));
+    }
+    
+}
+
---
+++ Inheritance_1/src/com/anon/Sub.java
@@ -0,0 +1,10 @@
+
+package com.anon;
+
+
+public class Sub extends Add{
+    
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+}
