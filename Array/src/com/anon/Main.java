dbddd58cda4152ce874a0b64520383be80d6e2af
Anon Bianco <anonh7@gmail.com>
2022-12-18 21:29:28 +0530

    a

---
+++ Array/src/com/anon/Main.java
@@ -0,0 +1,18 @@
+package com.anon;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0] = 25;
+        a[1] = 55;
+        a[2] = 88;
+        a[3] = 63;
+        a[4] = 66;
+        
+        System.out.println(a[3]);
+    }
+    
+}
